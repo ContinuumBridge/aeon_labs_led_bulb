02aba7c11cf36aeb8589b4cff56744ed4266f048
PeterClaydon <peter.claydon@continuumbridge.com>
2017-01-30 09:38:18 +0000

    WIP

--- adaptor_a.py
+++ adaptor_a.py
@@ -16,21 +16,21 @@ from cbconfig import *
 from twisted.internet import threads
 from twisted.internet import reactor
 
+colours = ["soft_white", "cold_white", "red", "green", "blue"]
+
 class Adaptor(CbAdaptor):
     def __init__(self, argv):
         self.status =           "ok"
         self.state =            "stopped"
         self.connected =        False
-        self.switchState =      "unknown"
-        self.apps =             {"energy": [],
-                                 "power": [],
-                                 "voltage": [],
-                                 "current": [],
-                                 "power_factor": [],
-                                 "binary_sensor": [],
-                                 "temperature": [],
-                                 "switch": [],
-                                 "connected": []}
+        self.apps =             {"rgbww": []}
+        self.colourState = {"soft_white": "0",
+            "cold_white": "0",
+            "red": "0",
+            "green": "0",
+            "blue": "0"
+        }
+        self.switchState =      "0"
         # super's __init__ must be called:
         #super(Adaptor, self).__init__(argv)
         CbAdaptor.__init__(self, argv)
@@ -60,136 +60,15 @@ class Adaptor(CbAdaptor):
         pass
 
     def pollSensors(self):
-        cmd = {"id": self.id,
-               "request": "post",
-               "address": self.addr,
-               "instance": "0",
-               "commandClass": "50",
-               "action": "Get",
-               "value": ""
-              }
-        self.sendZwaveMessage(cmd)
-        cmd = {"id": self.id,
-               "request": "post",
-               "address": self.addr,
-               "instance": "0",
-               "commandClass": "49",
-               "action": "Get",
-               "value": ""
-              }
-        self.sendZwaveMessage(cmd)
-        reactor.callLater(INTERVAL, self.pollSensors)
-
-    def checkConnected(self):
-        if self.updateTime == self.lastUpdateTime:
-            self.connected = False
-        else:
-            self.connected = True
-        self.sendCharacteristic("connected", self.connected, time.time())
-        self.lastUpdateTime = self.updateTime
-        reactor.callLater(INTERVAL + 10, self.checkConnected)
+        pass
 
     def onZwaveMessage(self, message):
         self.cbLog("debug", "onZwaveMessage, message: " + str(message))
         if message["content"] == "init":
             self.updateTime = 0
             self.lastUpdateTime = time.time()
-            # Energy - KWh
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "50",
-                   "value": "0"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Power - W
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "50",
-                   "value": "2"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Voltage
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "50",
-                   "value": "4"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Current - A
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "50",
-                   "value": "5"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Power Factor
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "50",
-                   "value": "6"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Temperature
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "49",
-                   "value": "1"
-                  }
-            self.sendZwaveMessage(cmd)
-            # Switch state
-            cmd = {"id": self.id,
-                   "request": "get",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "37",
-                   "value": "level"
-                  }
-            self.sendZwaveMessage(cmd)
-            reactor.callLater(30, self.pollSensors)
-            reactor.callLater(INTERVAL, self.checkConnected)
         elif message["content"] == "data":
             try:
-                if message["commandClass"] == "50":
-                    if message["value"] == "0":
-                        energy = message["data"]["val"]["value"] 
-                        self.sendCharacteristic("energy", energy, time.time())
-                    elif message["value"] == "2":
-                        power = message["data"]["val"]["value"] 
-                        self.sendCharacteristic("power", power, time.time())
-                    elif message["value"] == "4":
-                        voltage = message["data"]["val"]["value"] 
-                        self.sendCharacteristic("voltage", voltage, time.time())
-                    elif message["value"] == "5":
-                        current = message["data"]["val"]["value"] 
-                        self.sendCharacteristic("current", current, time.time())
-                    elif message["value"] == "6":
-                        power_factor = message["data"]["val"]["value"] 
-                        self.sendCharacteristic("power_factor", power_factor, time.time())
-                elif message["commandClass"] == "49":
-                    if message["value"] == "1":
-                        temperature = message["data"]["val"]["value"] 
-                        #self.cbLog("debug", "onZwaveMessage, temperature: " + str(temperature))
-                        self.sendCharacteristic("temperature", temperature, time.time())
-                elif message["commandClass"] == "37":
-                    if message["value"] == "level":
-                        if message["data"]["value"]:
-                            b = "on"
-                        else:
-                            b = "off"
-                        self.switchState = b
-                        self.sendCharacteristic("binary_sensor", b, time.time())
                 self.updateTime = message["data"]["updateTime"]
             except Exception as ex:
                 self.cbLog("warning", "onZwaveMessage, unexpected message: " + str(message))
@@ -201,14 +80,14 @@ class Adaptor(CbAdaptor):
         else:
             return "0"
 
-    def switch(self, onOrOff):
+    def switch(self, value):
         cmd = {"id": self.id,
                "request": "post",
                "address": self.addr,
                "instance": "0",
-               "commandClass": "0x25",
+               "commandClass": "0x33",
                "action": "Set",
-               "value": self.onOff(onOrOff)
+               "value": value 
               }
         self.sendZwaveMessage(cmd)
 
@@ -217,15 +96,7 @@ class Adaptor(CbAdaptor):
         resp = {"name": self.name,
                 "id": self.id,
                 "status": "ok",
-                "service": [{"characteristic": "energy", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "power", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "voltage", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "current", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "power_factor", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "connected", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "temperature", "interval": INTERVAL, "type": "switch"},
-                            {"characteristic": "binary_sensor", "interval": 0, "type": "switch"},
-                            {"characteristic": "switch", "interval": 0}],
+                "service": [{"characteristic": "led_rgbww", "interval": 0}],
                 "content": "service"}
         self.sendMessage(resp, message["id"])
         self.setState("running")
@@ -242,14 +113,16 @@ class Adaptor(CbAdaptor):
         self.cbLog("debug", "apps: " + str(self.apps))
 
     def onAppCommand(self, message):
-        self.cbLog("debug", "onAppCommand, req: " +  str(message))
+        #self.cbLog("debug", "onAppCommand, req: " +  str(message))
         if "data" not in message:
             self.cbLog("warning", "app message without data: " + str(message))
-        elif message["data"] != "on" and message["data"] != "off":
-            self.cbLog("warning", "switch state must be on or off: " +  str(message))
         else:
-            if message["data"] != self.switchState:
-                self.switch(message["data"])
+            for c in message["data"]:
+                if self.colourState[c] != message["data"][c]:
+                    self.colourState[c] = message["data"][c]
+                    value = str(colours.index(c)) + "," + message["data"][c]
+                    self.cbLog("debug", "onAppCommand, value: {}".format(value))
+                    self.switch(value)
 
     def onConfigureMessage(self, config):
         """Config is based on what apps are to be connected.
